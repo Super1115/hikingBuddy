08ddf6559c9efb358052f65750fa5a2bf7cc412a
Gronoxx <100954782+Gronoxx@users.noreply.github.com>
2024-11-09 00:17:27 -0300

    Update Arrow.jsx

--- src/components/Arrow.jsx
+++ src/components/Arrow.jsx
@@ -1,10 +1,7 @@
 import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 
-export function Arrow(props) {
-
-  const arrowRef = useRef();
-  
+export function Arrow(props) {  
 
   const { nodes, materials } = useGLTF("/models/direction_arrow.glb");
   return (
